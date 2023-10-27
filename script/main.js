278a4f6618a3355c17f2bdb18539aaa4b8db44fb
neckjs <98792546+junnn6ix@users.noreply.github.com>
2023-10-27 16:30:09 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -2,9 +2,17 @@ alert('km gd krjaan bgt mampir kesini');
 var nama = prompt('btw nm km siapa?');
 if(nama == "ajun" || nama == "AJUN"){
   alert('woy ' + nama + ' ganteng');
-}else{
-  alert('Hai ' + nama + '. meki');
 }
+if(nama == "Risma", "risma" || nama == "rsm", "rsma"){
+  alert('EH ASLI INI KM ' + nama + '?');
+  var tanya = prompt('ay lgsg jadi aja!');
+  if(tanya == true){
+    alert('YEEEEEEEE');
+  }else{
+   alert('yah');
+  }
+}
+alert('Hi ' + nama + ' slmt dtg!');
 
 // Menunggu dokumen HTML selesai dimuat
     document.addEventListener("DOMContentLoaded", function() {
